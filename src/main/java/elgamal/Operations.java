bb7f918adb6d5415d1e88feb706990b4078057c0
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-06 13:27:36 +0200

    Add getRandomNumberInRange method to operations

--- src/main/java/elgamal/Operations.java
+++ src/main/java/elgamal/Operations.java
@@ -1,5 +1,7 @@
 package elgamal;
 
+import java.util.Random;
+
 public class Operations {
     static int mod(String number, int a) {
         int output = 0;
@@ -8,5 +10,14 @@ public class Operations {
             output = (output * 10 + (int)number.charAt(i) - '0') % a;
         }
         return output;
+    }
+
+    public static int getRandomNumberInRange(int min, int max) {
+        if (min >= max) {
+            throw new IllegalArgumentException("max must be greater than min");
+        }
+
+        Random r = new Random();
+        return r.nextInt((max - min) + 1) + min;
     }
 }
