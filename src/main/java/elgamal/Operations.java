9ea9b1c5a165d3862da799bf7d48e129ed1a11de
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-06 12:52:29 +0200

    Add modulo method to operations class

--- src/main/java/elgamal/Operations.java
+++ src/main/java/elgamal/Operations.java
@@ -1,5 +1,12 @@
 package elgamal;
 
 public class Operations {
-//    TODO: Implement operations
+    static int mod(String number, int a) {
+        int output = 0;
+        
+        for (int i = 0; i < number.length(); i++)
+            output = (output * 10 + (int)number.charAt(i) - '0') % a;
+
+        return output;
+    }
 }
