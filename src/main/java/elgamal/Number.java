ab85ea60303d9797b45c9bbb8743f6a1d46b9d6a
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-16 20:16:36 +0200

    Finish number addition

--- src/main/java/elgamal/Number.java
+++ src/main/java/elgamal/Number.java
@@ -1,5 +1,7 @@
 package elgamal;
 
+import java.util.Arrays;
+
 public class Number {
 
     private byte[] digits;
@@ -21,21 +23,27 @@ public class Number {
 
     public Number add(Number number) {
         byte[] a = (digits.length >= number.getDigits().length ? digits : number.getDigits());
-        byte[] b = (digits.length < number.getDigits().length ? number.getDigits() : digits);
+        byte[] b = (digits.length >= number.getDigits().length ? number.getDigits() : digits);
         byte[] tmp = new byte[a.length + 1];
 
         byte carry = 0;
         for (int i = 0; i <= a.length; i++) {
-                if (b.length >= i - 1) {
-                    tmp[i] = (byte)((digits[i] + b[i] + carry) % 10);
-                    carry = (byte)((digits[i] + b[i] + carry) / 10);
+                if (b.length > i) {
+                    tmp[i] = (byte)((a[i] + b[i] + carry) % 10);
+                    carry = (byte)((a[i] + b[i] + carry) / 10);
+                } else if (a.length > i) {
+                    tmp[i] = (byte)((a[i] + carry) % 10);
+                    carry = (byte)((a[i] + carry) / 10);
                 } else {
-                    tmp[i] = (byte)(digits[i] + carry);
+                    tmp[i] = carry;
                     carry = 0;
                 }
         }
 
-        return new Number("0");
+        if (tmp[tmp.length - 1] == 0) {
+            tmp = Arrays.copyOfRange(tmp, 0, tmp.length - 1);
+        }
+        return new Number(tmp);
     }
 
     public byte[] getDigits() {
--- src/test/java/elgamal/NumberTest.java
+++ src/test/java/elgamal/NumberTest.java
@@ -19,6 +19,17 @@ class NumberTest {
     }
 
     @Test
+    void add() {
+        Number number1 = new Number("1111111111111111111111111");
+        Number number2 = new Number("22222222222222222222222222");
+        assertEquals("23333333333333333333333333", number1.add(number2).toString());
+
+        Number number3 = new Number("1");
+        Number number4 = new Number("99999999999999999999999999");
+        assertEquals("100000000000000000000000000", number3.add(number4).toString());
+    }
+
+    @Test
     void charToByte() {
         byte digit = Number.charToByte('1');
         assertEquals(1, digit);
