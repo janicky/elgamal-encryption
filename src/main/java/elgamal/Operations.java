c4a93bdd6554145b187a8bce8a3d8a32c9c29ed2
Patryk Janicki <kontakt.janicki@gmail.com>
2019-04-25 15:29:01 +0200

    Add generateBlocks method to operations

--- src/main/java/elgamal/Operations.java
+++ src/main/java/elgamal/Operations.java
@@ -1,31 +1,11 @@
 package elgamal;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Operations {
-    static int mod(String number, int a) {
-        int output = 0;
-        
-        for (int i = 0; i < number.length(); i++) {
-            output = (output * 10 + (int)number.charAt(i) - '0') % a;
-        }
-        return output;
-    }
-
-    public static long powmod(long a, long e, long n){
-        long accum = 1;
-        long x = e;
-        long apow = a;
-        while (x != 0){
-            if ((x & 0x01) == 0x01){
-                accum = (accum * apow) % n;
-            };
-            x >>= 1;
-            apow = (apow * apow) % n;
-        };
-        return accum;
-    }
-
     public static int getRandomNumberInRange(int min, int max) {
         if (min >= max) {
             throw new IllegalArgumentException("max must be greater than min");
@@ -33,5 +13,30 @@ public class Operations {
 
         Random r = new Random();
         return r.nextInt((max - min) + 1) + min;
+    }
+
+    public static Block[] generateBlocks(byte[] data, int length) {
+        List<Block> blocks = new ArrayList<>();
+        short[] split = new short[length];
+        int prepared = 0;
+        for (int i = 0; i < data.length; i++) {
+            split[i % length] = (short)((short)data[i] & 0xff);
+            prepared++;
+            if (prepared == length) {
+                blocks.add(new Block(split));
+                Arrays.fill(split, (short)0);
+                prepared = 0;
+            }
+        }
+        if (prepared > 0) {
+            blocks.add(new Block(split));
+        }
+
+        Block[] output = new Block[blocks.size()];
+        for (int i = 0; i < blocks.size(); i++) {
+            output[i] = blocks.get(i);
+        }
+
+        return output;
     }
 }
--- src/test/java/elgamal/OperationsTest.java
+++ src/test/java/elgamal/OperationsTest.java
@@ -2,13 +2,16 @@ package elgamal;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OperationsTest {
 
     @Test
-    void mod() {
-        assertEquals(0, Operations.mod("4", 2));
-        assertEquals(1, Operations.mod("1312312312321412413908123908131", 10));
+    void generateBlocks() {
+        byte[] data = new byte[] { 1, 2, 3, 15, 1, 2, 3, 16, (byte)255, 100, 99, 98, 2, 1, 1, 9, 5, 1 };
+        Block[] blocks = Operations.generateBlocks(data, 4 - 1);
+        System.out.println(Arrays.toString(blocks));
     }
 }
